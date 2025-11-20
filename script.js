ce435da6fdd07a55b35359f8adbcb2473d880cfb
Ashu-katariya <Ashishkatariya84@gmail.com>
2025-11-20 16:42:08 +0530

    Add smooth scroll to get started button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("TrackMyDSA loaded successfully");
+const btn = document.getElementById("get-started-btn");
+
+if (btn) {
+  btn.addEventListener("click", () => {
+    const featuresSection = document.getElementById("features");
+    if (featuresSection) {
+      featuresSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+}
