d7e50f6649de40513c1209ecd4aa3b96ab42c38c
Ashu-katariya <Ashishkatariya84@gmail.com>
2025-11-22 13:44:38 +0530

    Improve /health endpoint with extra info

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,13 @@ const PORT = 3000;
 
 // Simple route to test server
 app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
+  res.json({
+    status: "ok",
+    message: "TrackMyDSA backend is running",
+    time: new Date().toISOString(),
+  });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
